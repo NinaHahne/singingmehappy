09d052a95b122d40a607a3946153ecc4f58ba8cf
NinaHahne <ninamariehahne@gmail.com>
2025-11-12 00:40:30 +0100

    feat: refine styles and dimensions of RotatingBurgerMenuButton

--- src/lib/components/RotatingBurgerMenuButton.svelte
+++ src/lib/components/RotatingBurgerMenuButton.svelte
@@ -5,28 +5,28 @@
 
 <button
   id="toggle_menu"
-  class="menu-shadow fixed right-0 top-0 z-50 flex h-[70px] w-[80px] items-end justify-center p-[24px]"
+  class="menu-shadow fixed right-0 top-0 z-50 flex items-end justify-center p-[24px]"
   onclick={onToggle}
   aria-label={isOpen ? 'Close menu' : 'Open menu'}
 >
   <div
-    class="relative flex h-full w-full flex-col items-center justify-between transition-transform duration-300"
+    class="relative flex h-[26px] w-[38px] flex-col items-center justify-between transition-transform duration-300"
     class:rotate-180={isOpen}
   >
     <span
-      class="bg-softblack relative block h-[2px] w-8 rounded-sm transition-transform duration-300"
+      class="relative block h-[2px] w-full rounded-sm bg-softblack transition-transform duration-300"
       class:-rotate-45={isOpen}
-      class:translate-y-[10px]={isOpen}
+      class:translate-y-[12px]={isOpen}
     ></span>
     <span
-      class="bg-softblack relative block h-[2px] w-8 scale-100 rounded-sm transition-all duration-300"
+      class="relative block h-[2px] w-full scale-100 rounded-sm bg-softblack transition-all duration-300"
       class:opacity-0={isOpen}
       class:scale-0={isOpen}
     ></span>
     <span
-      class="bg-softblack relative block h-[2px] w-8 rounded-sm transition-transform duration-300"
+      class="relative block h-[2px] w-full rounded-sm bg-softblack transition-transform duration-300"
       class:rotate-45={isOpen}
-      class:-translate-y-[10px]={isOpen}
+      class:-translate-y-[12px]={isOpen}
     ></span>
   </div>
 </button>
