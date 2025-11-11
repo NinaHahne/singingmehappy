668f02a29e220b86dd4287f45ee96a611e913e5c
NinaHahne <ninamariehahne@gmail.com>
2025-11-11 16:13:48 +0100

    feat: deactivate font test section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,18 +19,13 @@
   <HeroSlider images={heroImages} interval={4500} firstInterval={1000} />
 </section>
 
-<section class="relative flex flex-col items-center gap-4 p-2 py-16 md:whitespace-nowrap">
+<!-- <section class="relative flex flex-col items-center gap-4 p-2 py-16 md:whitespace-nowrap">
   <div class="flex flex-col gap-2 text-4xl md:flex-row">
     <div class="mr-6 flex flex-col items-end gap-2">
       <h1 class="font-medium underline">Schriftarten-Vorschläge</h1>
       <div class="font-dosis">Jazzchor "Singing me happy" 2025</div>
       <div class="font-quicksand">Jazzchor "Singing me happy" 2025</div>
-      <!-- <div class="font-nunito-sans">Jazzchor "Singing me happy" 2025</div> -->
       <div class="font-montserrat">Jazzchor "Singing me happy" 2025</div>
-      <!-- <div class="font-didact-gothic">Jazzchor "Singing me happy" 2025</div> -->
-      <!-- <div class="font-exo2">"Singing me happy" 2025</div> -->
-      <!-- <div class="font-poppins">"Singing me happy" 2025</div> -->
-      <!-- <div class="font-rubik">"Singing me happy" 2025</div> -->
       <div class="font-dm-sans">Jazzchor "Singing me happy" 2025</div>
       <div class="font-josefin-sans">Jazzchor "Singing me happy" 2025</div>
     </div>
@@ -38,14 +33,9 @@
       <h1 class="font-light underline">Schriftname (leichter gesetzt)</h1>
       <div class="font-dosis">| Dosis (aktuell)</div>
       <div class="font-quicksand">| Quicksand</div>
-      <!-- <div class="font-nunito-sans">| Nunito Sans</div> -->
       <div class="font-montserrat">| Montserrat</div>
-      <!-- <div class="font-didact-gothic">| Didact Gothic</div> -->
-      <!-- <div class="font-exo2">| Exo 2</div> -->
-      <!-- <div class="font-poppins">| Poppins</div> -->
-      <!-- <div class="font-rubik">| Rubik</div> -->
       <div class="font-dm-sans">| DM Sans</div>
       <div class="font-josefin-sans">| Josefin Sans</div>
     </div>
   </div>
-</section>
+</section> -->
