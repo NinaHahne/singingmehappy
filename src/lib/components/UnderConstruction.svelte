8065d93d6cb7d85ce3a5000ab2cd8ad0025fca50
NinaHahne <ninamariehahne@gmail.com>
2025-11-16 19:25:07 +0100

    fix: adjust padding in UnderConstruction section for better layout

--- src/lib/components/UnderConstruction.svelte
+++ src/lib/components/UnderConstruction.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <HeaderAbsolute />
-<section class="flex flex-col items-center justify-center pt-20 text-center">
+<section class="flex flex-col items-center justify-center pt-28 text-center">
   <h1 class="text-black">{title}</h1>
   <p class="text-xl font-light italic text-gray-600 md:text-2xl">Diese Seite ist noch in Arbeit.</p>
 </section>
