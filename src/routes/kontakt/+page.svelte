95500217024e5bc3b756dc79e5a77c73bff17826
NinaHahne <ninamariehahne@gmail.com>
2025-11-19 16:56:33 +0100

    fix: adjust text styling for Kontakt page; remove unnecessary classes for consistency

--- src/routes/kontakt/+page.svelte
+++ src/routes/kontakt/+page.svelte
@@ -10,25 +10,32 @@
       <h1 class="text-4xl font-normal">Kontakt</h1>
     </div>
     <div class="max-w-3xl space-y-6">
-      <p class="max-w-2xl text-3xl font-light">
+      <p class="max-w-2xl">
         Du möchtest bei uns mitsingen? <br />
-        Schreib uns eine <a href="mailto:mitmachen@singingmehappy.de" class="text-3xl font-light">Mail</a>, in der du
-        uns etwas von deiner Chorerfahrung erzählst:
-        <a href="mailto:mitmachen@singingmehappy.de" class="text-3xl font-light">mitmachen@singingmehappy.de</a><br />
+        Schreib uns eine <a href="mailto:mitmachen@singingmehappy.de">Mail</a>, in der du uns etwas von deiner
+        Chorerfahrung erzählst:
+        <a href="mailto:mitmachen@singingmehappy.de">mitmachen@singingmehappy.de</a><br />
         Wir laden dich gern zu einer Probe ein.
       </p>
       <div>
         <h4 class="mb-0 text-3xl md:mb-0">Probenort:</h4>
-        <p class="text-3xl font-light">
+        <p>
           IBZ (Internationales BegegnungsZentrum der Wissenschaften) <br />Wiesbadener Str. 18<br />14197 Berlin -
           Wilmersdorf
         </p>
       </div>
-      <p class="text-3xl font-light">
+      <p>
         Du möchtest, dass wir auf deiner Veranstaltung singen? <br />
         Schreib uns an:
-        <a href="mailto:info@singingmehappy.de" class="text-3xl font-light">info@singingmehappy.de</a>
+        <a href="mailto:info@singingmehappy.de">info@singingmehappy.de</a>
       </p>
     </div>
   </section>
 </section>
+
+<style lang="postcss">
+  p,
+  a {
+    @apply text-2xl font-light md:text-3xl;
+  }
+</style>
