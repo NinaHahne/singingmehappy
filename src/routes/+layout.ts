c4bd8cf9e087b940f3a57160543c18c467596d72
NinaHahne <ninamariehahne@gmail.com>
2025-11-20 17:04:28 +0100

    feat: add trailingSlash configuration for consistent URL formatting

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,5 @@
 // This can be false if you're using a fallback (i.e. SPA mode)
 export const prerender = true;
+
+// Wichtig für Netcup/Apache: erzeugt /foo/index.html statt foo.html
+export const trailingSlash = 'always';
