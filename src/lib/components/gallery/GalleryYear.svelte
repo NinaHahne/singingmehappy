5345da5997a7c85e8fcdbd5f1935836fc6a2696d
NinaHahne <ninamariehahne@gmail.com>
2025-12-15 12:53:31 +0100

    fix: update z-index for improved layout consistency in gallery components

--- src/lib/components/gallery/GalleryYear.svelte
+++ src/lib/components/gallery/GalleryYear.svelte
@@ -8,9 +8,12 @@
   }>();
 </script>
 
+<!-- TODO: add scrollspy? -->
+<!-- TODO: replace isFirstYear and isLastYear logic -> css in @layer components -->
+
 <section id={`y_${year}`} class="year relative flex flex-col lg:flex-row lg:gap-8" class:!pt-0={isFirstYear}>
   <div
-    class="menu-shadow sticky top-20 mb-20 flex w-full flex-shrink-0 justify-end self-start text-6xl leading-none lg:top-[calc(50dvh-0.5em)] lg:my-[35px] lg:w-auto lg:justify-start lg:pr-3 lg:text-[100px] xl:text-[120px]"
+    class="menu-shadow sticky top-20 z-50 mb-20 flex w-full flex-shrink-0 justify-end self-start text-6xl leading-none lg:top-[calc(50dvh-0.5em)] lg:my-[35px] lg:w-auto lg:justify-start lg:pr-3 lg:text-[100px] xl:text-[120px]"
   >
     <span class="block w-[1em] lg:translate-x-[0.05em]">{year}</span>
   </div>
